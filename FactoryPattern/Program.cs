21283b3b22afdfa768359d3cab79e432c505ac5f
Rana <ranaberi25@gmail.com>
2024-03-06 20:23:39 +0100

    Add project files.

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,29 @@
+using FactoryPattern.Components;
+using FactoryPattern.Samples;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddTransient<ISample1, Sample1>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ FactoryPattern/Samples/Sample1.cs
@@ -0,0 +1,6 @@
+﻿namespace FactoryPattern.Samples;
+
+public class Sample1 : ISample1
+{
+    public string CurrentDateTime { get; set; } = DateTime.Now.ToString();
+}
